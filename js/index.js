b7ed41f301b4c71234ccf4186a981c3f814be1d1
Exequiel Pagliari <exepa@hotmail.com>
2023-05-18 15:50:13 -0300

    Agregado sistema de representación

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
-const app = Vue.createApp({
-    data(){
-        return{
 
-        }
-    },
-/*Cuando ubicamos el módulo de comunicación
-se debe crear en data() aquello que va a ser modificado.*/
+//Create const from buttom add and inputtext HTML / Crea las constantes del boton add e inputtext del HTML
+//Inicializate array ToDo / Inicializa el array ToDo
+const buttonAdd = document.getElementById("add");
+const textInput = document.getElementById("inputText")
+const Todo = []
 
 
-}).mount("#app")
+
+
+//Add textinput value to Array / Agrega valor de textimput a Array
+function addToDo() {
+    if(textInput.value.length > 0){
+        Todo.push(textInput.value)
+    }
+    
+}
+
+//Detect event in buttonAdd / Detecta evento en buttonAdd
+buttonAdd.addEventListener(
+    "click",
+    eventListener,
+    {});
+
+//Funtion from event / Funcion del evento
+function eventListener() {
+    
+    addToDo();
+    addPush()
+}
+//Renders the ToDo elements in the lang list / Representa los elementos de ToDo en la lista lang
+function addPush() {
+    eraselist("lang")
+    for (let index = 0; index < Todo.length; index++) {
+        var ul = document.getElementById("lang");
+        var li = document.createElement("li");
+        li.innerHTML = Todo[index]
+        ul.appendChild(li);
+    }
+}
+
+//Erase list contetn // Borra contenido de la lista
+function eraselist(listId) {
+    var ul = document.getElementById(`${listId}`);
+    ul.innerHTML = ""
+}
