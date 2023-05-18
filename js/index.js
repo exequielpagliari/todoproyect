f97ac8ed93af126d76c614ce1f862b48cf095b1c
Exequiel Pagliari <exepa@hotmail.com>
2023-05-18 00:03:51 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+
+        }
+    },
+/*Cuando ubicamos el módulo de comunicación
+se debe crear en data() aquello que va a ser modificado.*/
+
+
+}).mount("#app")
